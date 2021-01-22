106840c71235b2a0241d755516a1906997269ba4
Luciano Júnior <luciannojunior@gmail.com>
2021-01-22 07:42:50 -0300

    no-index plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-no-index`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
